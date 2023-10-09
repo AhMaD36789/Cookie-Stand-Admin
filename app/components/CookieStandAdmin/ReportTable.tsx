afaa0f634270ac4d27799aaf0c12d8dfff3092df
AhMaD36789 <ahmadsa28121999@gmail.com>
2023-10-10 02:30:05 +0300

    39 NO AUTH

--- app/components/CookieStandAdmin/ReportTable.tsx
+++ app/components/CookieStandAdmin/ReportTable.tsx
@@ -1,22 +1,77 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 type CookieStand = {
+  id?: number;
   location: string;
-  hourly_sales: number[];
+  hourlySales: number[];
+  description: string;
+  minimumCustomerPerHour: number;
+  maximumCustomerPerHour: number;
+  averageCookiesPerSale: number;
+  owner?: string;
 };
 
 type ReportTableProps = {
   hours: string[];
   reports: CookieStand[];
+  setReports: React.Dispatch<React.SetStateAction<CookieStand[]>>;
 };
 
-const ReportTable: React.FC<ReportTableProps> = ({ hours, reports }) => {
+const ReportTable: React.FC<ReportTableProps> = ({
+  hours,
+  reports,
+  setReports,
+}) => {
+  useEffect(() => {
+    console.log("Fetching data on page load...");
+    getAllData();
+  }, []);
+
+  const getAllData = async () => {
+    try {
+      const response = await axios.get(
+        `https://salmoncookieapplicationapi20231007222023.azurewebsites.net/api/CookieStands`
+      );
+      console.log("Fetched data:", response.data);
+      setReports(response.data); // Update reports with fetched data
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const deleteCookieStand = async (id: number | undefined) => {
+    if (!id) return; // Ensure id is not undefined
+    console.log("Deleting item with id:", id);
+    try {
+      await axios
+        .delete(
+          `https://salmoncookieapplicationapi20231007222023.azurewebsites.net/api/CookieStands/${id}`
+        )
+        .then(() => {
+          console.log("Deleted item, refreshing data...");
+          getAllData(); // Refresh data
+        })
+        .catch((error) => {
+          console.error("Error deleting item:", error);
+        });
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+
   if (reports.length === 0) {
     return <h2>No Cookie Stands Available</h2>;
   }
 
   const totalCookiesPerHour = hours.map((_, i) =>
-    reports.reduce((sum, report) => sum + report.hourly_sales[i], 0)
+    reports.reduce(
+      (sum, report) =>
+        sum + (Array.isArray(report.hourlySales) ? report.hourlySales[i] : 0),
+      0
+    )
   );
 
   const totalCookies = totalCookiesPerHour.reduce(
@@ -27,7 +82,7 @@ const ReportTable: React.FC<ReportTableProps> = ({ hours, reports }) => {
   return (
     <div className="flex justify-center items-center mb-10">
       <table className="block divide-gray-200">
-        <thead className="bg-green-500">
+        <thead className="bg-green-600">
           <tr>
             <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
               Location
@@ -45,22 +100,38 @@ const ReportTable: React.FC<ReportTableProps> = ({ hours, reports }) => {
             </th>
           </tr>
         </thead>
-        <tbody className="bg-green-300 divide-y divide-black">
+        <tbody className="divide-y divide-black">
           {reports.map((report, i) => (
-            <tr key={i}>
-              <td className="px-6 py-4 whitespace-nowrap">{report.location}</td>
-              {report.hourly_sales.map((sale, j) => (
-                <td key={j} className="px-6 py-4 whitespace-nowrap">
-                  {sale}
-                </td>
-              ))}
+            <tr
+              key={i}
+              className={i % 2 === 0 ? "bg-green-300" : "bg-green-500"}
+            >
+              <td className="px-6 py-4 whitespace-nowrap flex justify-between">
+                <div>{report.location}</div>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    onClick={() => report.id && deleteCookieStand(report.id)}
+                    className="hover:text-red-500 ml-5"
+                    width={10}
+                  />
+                </div>
+              </td>
+              {Array.isArray(report.hourlySales) &&
+                report.hourlySales.map((sale, j) => (
+                  <td key={j} className="px-6 py-4 whitespace-nowrap">
+                    {sale}
+                  </td>
+                ))}
               <td className="px-6 py-4 whitespace-nowrap">
-                {report.hourly_sales.reduce((a, b) => a + b, 0)}
+                {Array.isArray(report.hourlySales)
+                  ? report.hourlySales.reduce((a, b) => a + b, 0)
+                  : 0}
               </td>
             </tr>
           ))}
         </tbody>
-        <tfoot className="bg-green-500">
+        <tfoot className="bg-green-600">
           <tr>
             <td className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
               Totals
